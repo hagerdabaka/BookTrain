659b98a6fe7dd33a94c98e9548606f115b08afcb
hagerdabaka <hagerdabaka@gmail.com>
2024-02-06 21:35:54 +0200

    upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { IPassenger } from './models/stations';
+import { TrainService } from './Services/train.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+
+export class AppComponent {
+
+}
